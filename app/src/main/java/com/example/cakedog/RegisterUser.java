8c6f36749ae9454e0e2a46c17c63c110a98f8e11
Felipe de Lima <felipe.lima5611@gmail.com>
2023-09-22 01:42:41 -0300

    tela de registro

--- app/src/main/java/com/example/cakedog/RegisterUser.java
+++ app/src/main/java/com/example/cakedog/RegisterUser.java
@@ -1,7 +1,9 @@
 package com.example.cakedog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterUser extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class RegisterUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+    }
+
+    public void doRegister(View v) {
+        Intent change = new Intent(RegisterUser.this, MainActivity.class);
+        startActivity(change);
+        finish();
     }
 }
