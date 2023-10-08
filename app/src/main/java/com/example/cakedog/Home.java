ccb308f926d6f34c5b38be96e36c573e20ee80e7
Felipe de Lima <felipe.lima5611@gmail.com>
2023-10-08 19:33:33 -0300

    Modificações na Home

--- app/src/main/java/com/example/cakedog/Home.java
+++ app/src/main/java/com/example/cakedog/Home.java
@@ -8,13 +8,12 @@ import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 public class Home extends AppCompatActivity {
     ImageView img1, img2, img3, img4;
-    Button btnImgSimp, btnImgPers, btnImgTisko, btnImgEspec;
-    private ScrollView scrSimples, scrPers, scrTiskos, scrEspec, scrFirst;
-    private HorizontalScrollView srcHorizontal;
+    private ScrollView scrSimples, scrPers, scrTiskos, scrEspec;
     private Button btnSmp, btnPers, btnTiskos, btnEspec;
 
     @Override
@@ -30,16 +29,7 @@ public class Home extends AppCompatActivity {
         scrPers = findViewById(R.id.scrViewPers);
         scrTiskos = findViewById(R.id.scrViewTiskos);
         scrEspec = findViewById(R.id.scrViewEspecial);
-        srcHorizontal = findViewById(R.id.lnLayoutHorBar);
-        scrFirst = findViewById(R.id.scrViewFirst);
-        btnImgSimp = findViewById(R.id.btnToSimples);
-        btnImgPers = findViewById(R.id.btnToPers);
-        btnImgTisko = findViewById(R.id.btnToTiskos);
-        btnImgEspec = findViewById(R.id.btnToEspecial);
-        btnImgSimp.setBackgroundResource(R.drawable.bolo_simples);
-        btnImgPers.setBackgroundResource(R.drawable.bolo_pers2);
-        btnImgTisko.setBackgroundResource(R.drawable.caketisko);
-        btnImgEspec.setBackgroundResource(R.drawable.bolo_espec);
+        btnSelectorChanger();
     }
 
     public void showSimples(View v) {
@@ -92,10 +82,10 @@ public class Home extends AppCompatActivity {
             img2 = findViewById(R.id.imgSimples2);
             img3 = findViewById(R.id.imgSimples3);
             img4 = findViewById(R.id.imgSimples4);
-            img1.setImageResource(R.drawable.festa_doggo);
-            img2.setImageResource(R.drawable.festa_doggo);
-            img3.setImageResource(R.drawable.festa_doggo);
-            img4.setImageResource(R.drawable.festa_doggo);
+            img1.setImageResource(R.drawable.bolo_simples);
+            img2.setImageResource(R.drawable.bolo_simples);
+            img3.setImageResource(R.drawable.bolo_simples);
+            img4.setImageResource(R.drawable.bolo_simples);
         }
         else if(scrPers.getVisibility() == scrPers.VISIBLE) {
             btnPers.setBackgroundResource(R.drawable.btn_home_cake_selector_selected);
@@ -105,9 +95,11 @@ public class Home extends AppCompatActivity {
             img1 = findViewById(R.id.imgPers1);
             img2 = findViewById(R.id.imgPers2);
             img3 = findViewById(R.id.imgPers3);
-            img1.setImageResource(R.drawable.bolo_doggo);
-            img2.setImageResource(R.drawable.bolo_doggo);
-            img3.setImageResource(R.drawable.bolo_doggo);
+            img4 = findViewById(R.id.imgPers4);
+            img1.setImageResource(R.drawable.bolo_pers2);
+            img2.setImageResource(R.drawable.bolo_pers2);
+            img3.setImageResource(R.drawable.bolo_pers2);
+            img4.setImageResource(R.drawable.bolo_pers2);
         }
         else if(scrTiskos.getVisibility() == scrTiskos.VISIBLE) {
             btnTiskos.setBackgroundResource(R.drawable.btn_home_cake_selector_selected);
@@ -117,9 +109,11 @@ public class Home extends AppCompatActivity {
             img1 = findViewById(R.id.imgTiskos1);
             img2 = findViewById(R.id.imgTiskos2);
             img3 = findViewById(R.id.imgTiskos3);
-            img1.setImageResource(R.drawable.bolo_doggo);
-            img2.setImageResource(R.drawable.bolo_doggo);
-            img3.setImageResource(R.drawable.bolo_doggo);
+            img4 = findViewById(R.id.imgTiskos4);
+            img1.setImageResource(R.drawable.caketisko);
+            img2.setImageResource(R.drawable.caketisko);
+            img3.setImageResource(R.drawable.caketisko);
+            img4.setImageResource(R.drawable.caketisko);
         }
         else if(scrEspec.getVisibility() == scrEspec.VISIBLE) {
             btnEspec.setBackgroundResource(R.drawable.btn_home_cake_selector_selected);
@@ -129,16 +123,16 @@ public class Home extends AppCompatActivity {
             img1 = findViewById(R.id.imgEspec1);
             img2 = findViewById(R.id.imgEspec2);
             img3 = findViewById(R.id.imgEspec3);
-            img1.setImageResource(R.drawable.festa_doggo);
-            img2.setImageResource(R.drawable.festa_doggo);
-            img3.setImageResource(R.drawable.festa_doggo);
+            img4 = findViewById(R.id.imgEspec4);
+            img1.setImageResource(R.drawable.bolo_espec);
+            img2.setImageResource(R.drawable.bolo_espec);
+            img3.setImageResource(R.drawable.bolo_espec);
+            img4.setImageResource(R.drawable.bolo_espec);
         }
     }
 
-    public void toSimples(View v) {
-        scrSimples.setVisibility(View.VISIBLE);
-        scrFirst.setVisibility(View.GONE);
-        srcHorizontal.setVisibility(View.VISIBLE);
-        btnSelectorChanger();
+    public void toCart(View v) {
+        Intent change = new Intent(Home.this, ShoppingCart.class);
+        startActivity(change);
     }
 }
--- app/src/main/java/com/example/cakedog/ShoppingCart.java
+++ app/src/main/java/com/example/cakedog/ShoppingCart.java
@@ -3,9 +3,11 @@ package com.example.cakedog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ShoppingCart extends AppCompatActivity {
     ImageView img;
+    LinearLayout ln;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,6 @@ public class ShoppingCart extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_cart);
 
         img = findViewById(R.id.imgProdView);
+        img.setImageResource(R.drawable.bolo_espec);
     }
 }
